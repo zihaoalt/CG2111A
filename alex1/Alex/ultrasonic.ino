d8cf4a3d84e58c67bc48b802ee1407ecd5823e8a
w-chuhao <e1398566@u.nus.edu>
2025-04-19 13:25:31 +0800

    Update ultrasonic.ino

--- alex1/Alex/ultrasonic.ino
+++ alex1/Alex/ultrasonic.ino
@@ -1,28 +1,32 @@
 #include "constants.h"
 #include "packet.h"
 #include <math.h>
-#define TRIG 24
-#define ECHO 25
+#define TRIG 24 //PA2
+#define ECHO 25 //PA3
 #define TIMEOUT 4000
 #define SPEED_OF_SOUND 340 
 
 void setupUltrasonic() // Code for the ultrasonic sensor
 {
-  /*DDRD |= (1 << 7); // set trigger pin to output
-  PORTD &= ~(1 << 7); // write LOW to trigger pin
-  DDRD &= ~(1 << 6); // set echo pin to input*/
-  pinMode(TRIG, OUTPUT);
+  DDRA |= 0b00000100;
+  PORTA &= 0b11111011;
+  DDRA &= 0b11110111;
+  
+  /*pinMode(TRIG, OUTPUT);
   digitalWrite(TRIG, LOW);
   pinMode(ECHO, INPUT);
+  */
 }
 
 uint32_t readUltrasonic() { // detect distance of ultrasonic sensor from any objects in front of it
-  /*PORTD |= (1 << 7); // emit pulse from ultasonic sensor
-  delayMicroseconds(100); // delay 100 microseconds
-  PORTD &= ~(1 << 7); // stop emitting sound from ultrasonic sensor*/
-  digitalWrite(TRIG, HIGH);
+  /*digitalWrite(TRIG, HIGH);
   delayMicroseconds(100);
-  digitalWrite(TRIG, LOW);
+  digitalWrite(TRIG, LOW);*/
+  
+  PORTA |= 0b00000100;
+  delayMicroseconds(100);
+  PORTA &= 0b11111011;
+
   double duration = pulseIn(ECHO, HIGH, TIMEOUT); // measure time taken to detect echo from initial ultrasonic pulse
   double dist = duration / 2 / 10000 * SPEED_OF_SOUND; // calculate distance of object from ultrasonic sensor
   return (uint32_t) round(dist); // return distance of object from ultrasonic sensor in cm
